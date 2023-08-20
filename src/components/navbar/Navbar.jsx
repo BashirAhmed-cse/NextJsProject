1d563115c63713f54324614a75601f52bd19dd68
bashir <bashirahmed.cse@gmail.com>
2023-08-20 22:44:00 +0600

    update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,33 @@
+import Link from 'next/link'
 import React from 'react'
+
+ const links = [
+    {
+        id:1,
+        title:"Home",
+        url:"/",
+    },
+    {
+        id:1,
+        title:"Home",
+        url:"/",
+    },
+    {
+        id:1,
+        title:"Home",
+        url:"/",
+    },
+    {
+        id:1,
+        title:"Home",
+        url:"/",
+    },
+ ]
 
 function Navbar() {
   return (
     <div>
-      Navbar
+       <Link href="/">Bashir</Link>
     </div>
   )
 }
