f5498bf562355cba11be3d1ff70186b515a2abb6
bashir <bashirahmed.cse@gmail.com>
2023-08-21 22:20:41 +0600

    update contact page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './page.module.css'
 import Image from 'next/image'
+import Button from '@/components/button/Button'
 function About() {
   return (
     <div className={styles.container}>
@@ -33,9 +34,21 @@ function About() {
          </div>
          <div className={styles.item}>
          <h1 className={styles.title}>What We Do?</h1>
+         <p className={styles.desc}>Lorem Ipsum is simply dummy text of the printing and typesetting 
+            industry. Lorem Ipsum has been the industry's standard dummy text 
+            ever since the 1500s, when an unknown printer took a galley of type 
+            and scrambled it to make a type specimen book. It has survived not 
+            <br/>
+            <br/>
+            only five centuries, but also the leap into electronic typesetting, 
+            remaining essentially unchanged. It was popularised in the 1960s 
+            with the release of Letraset sheets containing Lorem Ipsum passages, 
+            and more recently with desktop publishing software like Aldus 
+            PageMaker including versions of Lorem Ipsum</p>
+            <Button url="/contact" text="Contact"/>
+         </div>
          </div>
        </div>
-    </div>
   )
 }
 
