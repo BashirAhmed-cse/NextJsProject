9689cddcaa24bea94e5354f27d939f9add1ac860
bashirahmed-cse <bashirahmed.cse@gmail.com>
2023-08-22 10:57:05 +0600

    update

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,9 +1,56 @@
 import React from 'react'
-
-function Category() {
+import styles from './page.module.css'
+import Button from '@/components/button/Button'
+import Image from 'next/image'
+const Category = ({params}) => {
   return (
-    <div>
-       Category
+    <div className={styles.container}>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+      <div className={styles.item}>
+      <div className={styles.content}>
+         <h1 className={styles.title}>Test</h1>
+         <p className={styles.desc}>Desc</p>
+         <Button text="See More" url="#"/>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image
+        className={styles.img}
+         fill={true}
+         src="https://images.pexels.com/photos/12181253/pexels-photo-12181253.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+         alt=""
+        />
+      </div>
+      </div>
+      <div className={styles.item}>
+      <div className={styles.content}>
+         <h1 className={styles.title}>Test</h1>
+         <p className={styles.desc}>Desc</p>
+         <Button text="See More" url="#"/>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image
+        className={styles.img}
+         fill={true}
+         src="https://images.pexels.com/photos/12181253/pexels-photo-12181253.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+         alt=""
+        />
+      </div>
+      </div>
+      <div className={styles.item}>
+      <div className={styles.content}>
+         <h1 className={styles.title}>Test</h1>
+         <p className={styles.desc}>Desc</p>
+         <Button text="See More" url="#"/>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image
+        className={styles.img}
+         fill={true}
+         src="https://images.pexels.com/photos/12181253/pexels-photo-12181253.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+         alt=""
+        />
+      </div>
+      </div>
     </div>
   )
 }
